6e522dfcc01306dcdb81f728ab60040a30985d86
Luiz Guilherme Pelegrini <ldesouzapelegrini01@gmail.com>
2022-03-17 16:04:52 +0800

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,27 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import serviceAccount from './serviceAccount.json';
+import admin from 'firebase-admin';
+
+// fires up Firebase admin
+const firebaseAdmin = admin.initializeApp({
+  databaseURL: '<database_url>',
+  credential: admin.credential.cert({
+    clientEmail: serviceAccount.client_email,
+    privateKey: serviceAccount.private_key,
+    projectId: serviceAccount.project_id,
+  }),
+});
+
+export const handler = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  
+  // do something with firebase admin...
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello World'
+    }),
+  };
+};
